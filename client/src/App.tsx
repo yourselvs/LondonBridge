8e6f398728083e512f6f9393cb5a18f74fe475b8
Michael <yourselvs@live.com>
2024-10-07 23:57:48 -0400

    Use React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactsfcesfs
-        </a>
-      </header>
-    </div>
-  );
+const About: React.FC = () => {
+  return (<>
+  <Link to="/">Home</Link> <Link to="/about">About</Link> <Link to="/news">News</Link>
+      <div>About</div>
+  </>);
+};
+
+const Home: React.FC = () => {
+  return (<><Link to="/">Home</Link> <Link to="/about">About</Link> <Link to="/news">News</Link>
+    <div>Home</div>
+  </>);
+};
+
+const News: React.FC = () => {
+  return (<><Link to="/">Home</Link> <Link to="/about">About</Link> <Link to="/news">News</Link>
+    <div>News</div>
+  </>);
+};
+
+const App: React.FC = () => {
+  return (<>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />}/>
+            <Route path="/news" element={<News />} />
+        </Routes>
+    </BrowserRouter>
+  </>);
 }
 
 export default App;
